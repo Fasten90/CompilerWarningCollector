0f558b6e7f1056c4c4960a29891dcae8ddf7abe6
Fasten90 <fasten90@gmail.com>
2019-08-20 19:43:32 +0200

    # MSVC: First run version

---
+++ CompilerWarningCollector.py
@@ -0,0 +1,76 @@
+
+import re
+
+r"""
+MSVC
+C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0\ucrt\stdio.h(948,37): warning C4710:  'int printf(const char *const ,...)': function not inlined [D:\a\1\s\Out\CMakeBuild\FastenHomeAut.vcxproj]
+  EventHandler.c
+  EventLog.c
+  GlobalVarHandler.c
+  HomeAutMessage.c
+
+D:\a\1\s\Src\Communication\Communication.c(223,1): warning C4206:  nonstandard extension used: translation unit is empty [D:\a\1\s\Out\CMakeBuild\FastenHomeAut.vcxproj]
+  WebpageHandler.c
+  Display.c
+  DisplayHandler.c
+  DisplayImages.c
+  Display_SSD1306.c
+  Font12x8.c
+  Generating Code...
+"""
+
+r"""
+
+D:\a\1\s\Src\SelfTest\SelfTest_Errors.c: In function 'SelfTest_Errors_MemFault':
+D:\a\1\s\Src\SelfTest\SelfTest_Errors.c:98:31: warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
+     uint32_t * wrongPointer = (uint32_t *)constValue; /* Set pointer address to an incorrect address */
+                               ^
+"""
+
+
+
+
+COMPILER_WARNING_CHECKERS = [
+    {
+        "CompilerName": "MSVC",
+        "AutoDetect": None,
+        "WarningChecker": r"(?P<FilePath>[\:\\\/\w\s\(\)\_\-\.]+)[\\|\/](?P<FileName>[\w\_\-]+\.[\w]*)\((?P<LineNumber>[\d]+),(?P<ColumnIndex>[\d]+)\)\: warning C(?P<WarningId>[\d]+)\: (?P<WarningMessage>[^[]*)\["
+
+    }
+
+]
+
+
+def check_text(text, compiler="MSVC"):
+    regex_warning = re.compile(COMPILER_WARNING_CHECKERS[0]["WarningChecker"])
+
+    # TOOD: Delete
+    #for i, line in self.__file_content_enumerated_list:
+    #    result = regex_text_full_line.match(line)
+
+    match = re.findall(regex_warning, text)
+
+    for m in regex_warning.finditer(text):
+        print(m.groupdict())
+
+"""
+    if match:
+        for oneMatch in match:
+            line = oneMatch.group("Line")
+            print("Line: {}".format(line))
+"""
+
+
+
+
+if __name__== "__main__":
+    text = \
+r"""
+C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0\ucrt\stdio.h(948,37): warning C4710:  'int printf(const char *const ,...)': function not inlined [D:\a\1\s\Out\CMakeBuild\FastenHomeAut.vcxproj]
+  EventHandler.c
+  EventLog.c
+  GlobalVarHandler.c
+  HomeAutMessage.c
+"""
+    check_text(text, "MSVC")
+
